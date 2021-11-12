511383a8e1c27b4df98be59c8bce8d27f7b5304f
mizan <mizanur919@gmail.com>
2021-11-13 05:18:36 +0600

    responsive

--- src/Pages/AllProducts/AllProducts/AllProducts.js
+++ src/Pages/AllProducts/AllProducts/AllProducts.js
@@ -17,7 +17,7 @@ const AllProducts = () => {
             <div className="p-5" id="services">
                 <div className="container">
                     <h2 className="text-center mb-5">All Products</h2>
-                    <div className="row row-cols-1 row-cols-md-4 g-4">
+                    <div className="row row-cols-1 row-cols-lg-4 row-cols-md-2 g-4">
                         {
                             allProducts.map(product => <SingleAllProducts
                                 key={product._id}
